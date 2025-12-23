661d894078e5821039e2d3e0b33c9eeb3208960e
wdog5734 <1798643961@qq.com>
2025-12-23 10:39:42 +0800

    feat:some mixins and patches

---
+++ src/main/java/org/celestial_artistry/neotenet/mixin/world/entity/ai/behavior/MixinGoToWantedItem.java
@@ -0,0 +1,37 @@
+package org.celestial_artistry.neotenet.mixin.world.entity.ai.behavior;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.behavior.GoToWantedItem;
+import net.minecraft.world.entity.ai.behavior.declarative.BehaviorBuilder;
+import net.minecraft.world.entity.ai.behavior.declarative.MemoryAccessor;
+import net.minecraft.world.entity.item.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
+
+@Mixin(GoToWantedItem.class)
+public class MixinGoToWantedItem {
+
+    @Inject(method = "lambda$create$1", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/behavior/declarative/MemoryAccessor;set(Ljava/lang/Object;)V"), cancellable = true)
+    private static void neotenet$gotoEvent(BehaviorBuilder.Instance p_258371_, MemoryAccessor p_258389_, MemoryAccessor p_258390_, Predicate p_259490_, int p_259054_, float p_260346_, MemoryAccessor p_258387_, MemoryAccessor p_258388_, ServerLevel p_258380_, LivingEntity p_258381_, long p_258382_, CallbackInfoReturnable<Boolean> cir, @Local(name = "itementity") ItemEntity itementity) {
+        // CraftBukkit start
+        if (p_258381_ instanceof net.minecraft.world.entity.animal.allay.Allay) {
+            org.bukkit.event.entity.EntityTargetEvent event = org.bukkit.craftbukkit.event.CraftEventFactory.callEntityTargetEvent(p_258381_, itementity, org.bukkit.event.entity.EntityTargetEvent.TargetReason.CLOSEST_ENTITY);
+
+            if (event.isCancelled()) {
+                cir.setReturnValue(false);
+            }
+            if (!(event.getTarget() instanceof ItemEntity)) {
+                p_258389_.erase();
+            }
+
+            itementity = (ItemEntity) ((org.bukkit.craftbukkit.entity.CraftEntity) event.getTarget()).getHandle();
+        }
+        // CraftBukkit end
+    }
+}
---
+++ src/main/java/org/celestial_artistry/neotenet/mixin/world/entity/ai/behavior/MixinInteractWithDoor.java
@@ -0,0 +1,30 @@
+package org.celestial_artistry.neotenet.mixin.world.entity.ai.behavior;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.behavior.InteractWithDoor;
+import net.minecraft.world.entity.ai.behavior.declarative.BehaviorBuilder;
+import net.minecraft.world.entity.ai.behavior.declarative.MemoryAccessor;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.lang3.mutable.MutableObject;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(InteractWithDoor.class)
+public class MixinInteractWithDoor {
+
+    @Inject(method = "lambda$create$3", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/DoorBlock;setOpen(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Z)V", ordinal = 0), cancellable = true)
+    private static void neotenet$interactEvent(BehaviorBuilder.Instance p_258474_, MemoryAccessor p_258460_, MemoryAccessor p_258461_, MutableObject mutableobject, MutableInt mutableint, MemoryAccessor p_258462_, ServerLevel p_258469_, LivingEntity p_258470_, long p_258471_, CallbackInfoReturnable<Boolean> cir, @Local(name = "blockpos") BlockPos blockpos) {
+        // CraftBukkit start - entities opening doors
+        org.bukkit.event.entity.EntityInteractEvent event = new org.bukkit.event.entity.EntityInteractEvent(p_258470_.getBukkitEntity(), org.bukkit.craftbukkit.block.CraftBlock.at(p_258470_.level(), blockpos));
+        p_258470_.level().getCraftServer().getPluginManager().callEvent(event);
+        if (event.isCancelled()) {
+            cir.setReturnValue(false);
+        }
+        // CraftBukkit end
+    }
+}
