d4b052b4805ae14b368dc749e1dda94d1c992895
wdog5734 <1798643961@qq.com>
2025-12-23 15:11:36 +0800

    feat:more mixins and patches

---
+++ src/main/java/org/celestial_artistry/neotenet/mixin/world/level/block/MixinBambooStalkBlock.java
@@ -0,0 +1,19 @@
+package org.celestial_artistry.neotenet.mixin.world.level.block;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.level.block.BambooStalkBlock;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BambooStalkBlock.class)
+public class MixinBambooStalkBlock {
+
+    @ModifyExpressionValue(method = "performBonemeal", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getValue(Lnet/minecraft/world/level/block/state/properties/Property;)Ljava/lang/Comparable;"))
+    private Comparable neotenet$noBamboo(Comparable original, @Local(name = "blockstate") BlockState blockstate) {
+        return !blockstate.is(Blocks.BAMBOO);
+    }
+}
+
