f694a55378b9446d7b5229e27137100d2a2621a3
wdog5734 <1798643961@qq.com>
2025-12-27 00:57:41 +0800

    fix:fix mixins

--- src/main/java/org/teneted/neotenet/mixin/world/level/block/MixinBaseCommandBlock.java
+++ src/main/java/org/teneted/neotenet/mixin/world/level/block/MixinBaseCommandBlock.java
@@ -1,12 +1,10 @@
 package org.teneted.neotenet.mixin.world.level.block;
 
-import com.google.common.base.Joiner;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.BaseCommandBlock;
-import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.CraftServer;
-import org.bukkit.event.server.ServerCommandEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -15,35 +13,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public abstract class MixinBaseCommandBlock {
 
     @Redirect(method = "performCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/Commands;performPrefixedCommand(Lnet/minecraft/commands/CommandSourceStack;Ljava/lang/String;)V"))
-    private void neotenet$serverCommand(Commands commands, CommandSourceStack sender, String command) {
-        Joiner joiner = Joiner.on(" ");
-        if (command.startsWith("/")) {
-            command = command.substring(1);
-        }
-
-        ServerCommandEvent event = new ServerCommandEvent(sender.getBukkitSender(), command);
-        Bukkit.getPluginManager().callEvent(event);
-        if (event.isCancelled()) {
-            return;
-        }
-        command = event.getCommand();
-
-        String[] args = command.split(" ");
-
-        String cmd = args[0];
-        if (cmd.startsWith("minecraft:")) cmd = cmd.substring("minecraft:".length());
-        if (cmd.startsWith("bukkit:")) cmd = cmd.substring("bukkit:".length());
-
-        if (cmd.equalsIgnoreCase("stop") || cmd.equalsIgnoreCase("kick") || cmd.equalsIgnoreCase("op")
-                || cmd.equalsIgnoreCase("deop") || cmd.equalsIgnoreCase("ban") || cmd.equalsIgnoreCase("ban-ip")
-                || cmd.equalsIgnoreCase("pardon") || cmd.equalsIgnoreCase("pardon-ip") || cmd.equalsIgnoreCase("reload")) {
-            return;
-        }
-
-        if (((CraftServer) Bukkit.getServer()).getCommandBlockOverride(args[0])) {
-            args[0] = "minecraft:" + args[0];
-        }
-
-        commands.performPrefixedCommand(sender, joiner.join(args));
+    private void neotenet$serverCommand(Commands commands, CommandSourceStack sender, String command, @Local MinecraftServer minecraftserver) {
+        minecraftserver.getCommands().dispatchServerCommand(sender, command);
     }
 }
