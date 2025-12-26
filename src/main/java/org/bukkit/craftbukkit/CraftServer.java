26cc1e91197c4ac9ad43a10906705317eb82f35f
wdog5734 <1798643961@qq.com>
2025-12-27 00:44:07 +0800

    fix:fix missing patches

--- src/main/java/org/bukkit/craftbukkit/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/CraftServer.java
@@ -596,14 +596,6 @@ public final class CraftServer implements Server {
 
     @Override
     public List<CraftPlayer> getOnlinePlayers() {
-        // NeoTenet start - refresh online players
-        this.playerView = Collections.unmodifiableList(Lists.transform(playerList.players, new Function<ServerPlayer, CraftPlayer>() {
-            @Override
-            public CraftPlayer apply(ServerPlayer player) {
-                return player.getBukkitEntity();
-            }
-        }));
-        // NeoTenet end
         return this.playerView;
     }
 
