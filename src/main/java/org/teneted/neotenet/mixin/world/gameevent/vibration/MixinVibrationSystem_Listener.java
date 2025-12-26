261276239a7539062c75334cd4b160bf7a67410d
wdog5734 <1798643961@qq.com>
2025-12-26 11:14:22 +0800

    fix:fix MixinVibrationSystem_Listener

--- src/main/java/org/teneted/neotenet/mixin/world/gameevent/vibration/MixinVibrationSystem_Listener.java
+++ src/main/java/org/teneted/neotenet/mixin/world/gameevent/vibration/MixinVibrationSystem_Listener.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(VibrationSystem.Listener.class)
-public interface MixinVibrationSystem_Listener {
+public class MixinVibrationSystem_Listener {
 
     @ModifyExpressionValue(method = "handleGameEvent", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/gameevent/vibrations/VibrationSystem$User;canReceiveVibration(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Holder;Lnet/minecraft/world/level/gameevent/GameEvent$Context;)Z"))
     private boolean neotenet$handleGameEvent(boolean original, @Local(argsOnly = true) ServerLevel p_282254_, @Local(argsOnly = true) Holder<GameEvent> p_316513_, @Local(argsOnly = true) GameEvent.Context p_283664_, @Local(argsOnly = true) Vec3 p_282426_) {
