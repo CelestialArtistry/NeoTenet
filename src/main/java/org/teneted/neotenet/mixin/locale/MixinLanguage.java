95a1f5e8cc1b4b398df10b77f420bcb5f233f189
wdog5734 <1798643961@qq.com>
2025-12-29 13:25:10 +0800

    feat:npc patches

--- src/main/java/org/teneted/neotenet/mixin/locale/MixinLanguage.java
+++ src/main/java/org/teneted/neotenet/mixin/locale/MixinLanguage.java
@@ -2,14 +2,12 @@ package org.teneted.neotenet.mixin.locale;
 
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
-import net.neoforged.neoforge.common.NeoForgeMod;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.teneted.neotenet.config.NeoTenetConfig;
 import org.teneted.neotenet.config.NeoTenetConfigUtil;
 
 import java.io.InputStream;
@@ -29,7 +27,7 @@ public abstract class MixinLanguage {
 
     @Redirect(method = "parseTranslations(Ljava/util/function/BiConsumer;Ljava/util/function/BiConsumer;Ljava/lang/String;)V", at = @At(value = "INVOKE", target = "Ljava/lang/Class;getResourceAsStream(Ljava/lang/String;)Ljava/io/InputStream;"))
     private static InputStream neotenet$useUserLanguage(Class instance, String e) {
-        return NeoForgeMod.class.getResourceAsStream("/assets/minecraft/lang/%s.json".formatted(NeoTenetConfigUtil.lang().toLowerCase(Locale.ROOT)));
+        return Language.class.getResourceAsStream("/assets/minecraft/lang/%s.json".formatted(NeoTenetConfigUtil.lang().toLowerCase(Locale.ROOT)));
     }
 }
 
---
+++ src/main/java/org/teneted/neotenet/mixin/world/entity/npc/MixinInventoryCarrier.java
@@ -0,0 +1,33 @@
+package org.teneted.neotenet.mixin.world.entity.npc;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.npc.InventoryCarrier;
+import net.minecraft.world.item.ItemStack;
+import org.bukkit.event.entity.EntityRemoveEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InventoryCarrier.class)
+public class MixinInventoryCarrier {
+
+    @Inject(method = "pickUpItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Mob;onItemPickup(Lnet/minecraft/world/entity/item/ItemEntity;)V"), cancellable = true)
+    private static void neotenet$callEntityPickupItemEvent(Mob p_219612_, InventoryCarrier p_219613_, ItemEntity p_219614_, CallbackInfo ci, @Local SimpleContainer simplecontainer, @Local ItemStack itemstack) {
+        // CraftBukkit start
+        ItemStack remaining = new SimpleContainer(simplecontainer).addItem(itemstack);
+        if (org.bukkit.craftbukkit.event.CraftEventFactory.callEntityPickupItemEvent(p_219612_, p_219614_, remaining.getCount(), false).isCancelled()) {
+            ci.cancel();
+            return;
+        }
+        // CraftBukkit end
+    }
+
+    @Inject(method = "pickUpItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;discard()V"))
+    private static void neotenet$discardReason(Mob p_219612_, InventoryCarrier p_219613_, ItemEntity p_219614_, CallbackInfo ci) {
+        p_219614_.pushRemoveCause(EntityRemoveEvent.Cause.PICKUP);
+    }
+}
