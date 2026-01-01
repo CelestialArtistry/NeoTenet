d7dd4e546a5ed0a232a91d1e89276e6e7fb48760
wdog5734 <1798643961@qq.com>
2026-01-01 22:29:27 +0800

    feat:some block patches

---
+++ src/main/java/org/teneted/neotenet/mixin/world/level/block/MixinBaseFireBlock.java
@@ -0,0 +1,39 @@
+package org.teneted.neotenet.mixin.world.level.block;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseFireBlock;
+import net.minecraft.world.level.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.teneted.neotenet.injection.world.level.block.InjectionBaseFireBlock;
+
+@Mixin(BaseFireBlock.class)
+public class MixinBaseFireBlock implements InjectionBaseFireBlock {
+
+    @Redirect(method = "entityInside", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;igniteForSeconds(F)V"))
+    private void neotenet$callEntityCombustByBlockEvent(Entity instance, float v,
+                                                        @Local(argsOnly = true) Level p_49261_,
+                                                        @Local(argsOnly = true) BlockPos p_49262_) {
+        // CraftBukkit start
+        org.bukkit.event.entity.EntityCombustEvent event = new org.bukkit.event.entity.EntityCombustByBlockEvent(org.bukkit.craftbukkit.block.CraftBlock.at(p_49261_, p_49262_), instance.getBukkitEntity(), 8.0F);
+        p_49261_.getCraftServer().getPluginManager().callEvent(event);
+
+        if (!event.isCancelled()) {
+            instance.igniteForSeconds(event.getDuration(), false);
+        }
+        // CraftBukkit end
+    }
+
+    // CraftBukkit start
+    @Override
+    public void fireExtinguished(net.minecraft.world.level.LevelAccessor world, BlockPos position) {
+        if (!org.bukkit.craftbukkit.event.CraftEventFactory.callBlockFadeEvent(world, position, Blocks.AIR.defaultBlockState()).isCancelled()) {
+            world.removeBlock(position, false);
+        }
+    }
+    // CraftBukkit end
+}
